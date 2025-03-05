51a16aa7f8e2122a6413a59d789363ad1184d7e8
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-05 08:19:51 -0500

    feat(web): showcase buttons

--- src/web/web.tsx
+++ src/web/web.tsx
@@ -1,8 +1,35 @@
+import { MdAdd } from 'react-icons/md';
+import { Button } from '../components/button';
+
 export function Web() {
   return (
-    <div>
-      <h1>Web - Alarmas Pico Placa</h1>
-      <p>Texto de prueba</p>
+    <div style={{ padding: '1rem' }}>
+      <h1 className="text--display-large">Web - Alarmas Pico Placa</h1>
+      <p>Listado de botones:</p>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button text="Label" />
+        <Button disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button icon={MdAdd} text="Label" />
+        <Button icon={MdAdd} disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button variant="outlined" text="Label" />
+        <Button variant="outlined" disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button icon={MdAdd} variant="outlined" text="Label" />
+        <Button icon={MdAdd} variant="outlined" disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button variant="inline" text="Label" />
+        <Button variant="inline" disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button icon={MdAdd} variant="inline" text="Label" />
+        <Button icon={MdAdd} variant="inline" disabled text="Label" />
+      </div>
     </div>
   );
 }
