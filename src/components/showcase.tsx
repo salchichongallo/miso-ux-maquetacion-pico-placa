cbd6e162266348eaef1a0afedbb0880b3063905c
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-05 21:47:29 -0500

    refactor: reuse showcase

---
+++ src/components/showcase.tsx
@@ -0,0 +1,77 @@
+import { MdOutlineCancel, MdArrowBack, MdAdd } from 'react-icons/md';
+
+import { Button } from './button/button';
+import { RadioGroup, Radio } from './radio';
+import { TextField } from './text-field/text-field';
+import { IconButton } from './icon-button/icon-button';
+
+type Props = {
+  title: string;
+};
+
+export function Showcase({ title }: Props) {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1 className="text--display-large">{title}</h1>
+      <p>Listado de componentes:</p>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <TextField
+          label="Label"
+          placeholder="Input"
+          description="Texto de soporte"
+          trailingIcon={<IconButton icon={MdOutlineCancel} />}
+        />
+        <TextField
+          label="Label"
+          placeholder="Input"
+          errorMessage="Texto de soporte"
+          isInvalid
+        />
+      </div>
+      <div
+        style={{
+          marginTop: '1rem',
+          display: 'flex',
+          gap: '1rem',
+        }}
+      >
+        <RadioGroup label="Choose your pet" name="pet" defaultValue="orni">
+          <Radio value="dogs">Dog</Radio>
+          <Radio value="cats">Cat</Radio>
+          <Radio value="dragon">Dragon</Radio>
+          <Radio isDisabled value="orni">
+            Ortinorrinco
+          </Radio>
+        </RadioGroup>
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <IconButton icon={MdArrowBack} />
+        <IconButton icon={MdArrowBack} disabled />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button text="Label" />
+        <Button disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button icon={MdAdd} text="Label" />
+        <Button icon={MdAdd} disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button variant="outlined" text="Label" />
+        <Button variant="outlined" disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button icon={MdAdd} variant="outlined" text="Label" />
+        <Button icon={MdAdd} variant="outlined" disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button variant="inline" text="Label" />
+        <Button variant="inline" disabled text="Label" />
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <Button icon={MdAdd} variant="inline" text="Label" />
+        <Button icon={MdAdd} variant="inline" disabled text="Label" />
+      </div>
+    </div>
+  );
+}
--- src/mobile/mobile.tsx
+++ src/mobile/mobile.tsx
@@ -1,35 +1,5 @@
-import { MdAdd } from 'react-icons/md';
-import { Button } from '../components/button/button';
+import { Showcase } from '../components/showcase';
 
 export function Mobile() {
-  return (
-    <div style={{ padding: '1rem' }}>
-      <h1 className="text--display-large">Mobile - Alarmas Pico Placa</h1>
-      <p>Listado de botones:</p>
-      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
-        <Button text="Label" />
-        <Button disabled text="Label" />
-      </div>
-      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
-        <Button icon={MdAdd} text="Label" />
-        <Button icon={MdAdd} disabled text="Label" />
-      </div>
-      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
-        <Button variant="outlined" text="Label" />
-        <Button variant="outlined" disabled text="Label" />
-      </div>
-      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
-        <Button icon={MdAdd} variant="outlined" text="Label" />
-        <Button icon={MdAdd} variant="outlined" disabled text="Label" />
-      </div>
-      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
-        <Button variant="inline" text="Label" />
-        <Button variant="inline" disabled text="Label" />
-      </div>
-      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
-        <Button icon={MdAdd} variant="inline" text="Label" />
-        <Button icon={MdAdd} variant="inline" disabled text="Label" />
-      </div>
-    </div>
-  );
+  return <Showcase title="Mobile - Alarmas Pico Placa" />;
 }
