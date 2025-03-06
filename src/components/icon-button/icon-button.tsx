cf3b56f9f3b8050f71974c05bf5a625ee59e85d0
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-05 21:35:43 -0500

    feat(icon-button): allow to override icon color

--- src/components/icon-button/icon-button.tsx
+++ src/components/icon-button/icon-button.tsx
@@ -5,6 +5,7 @@ import './icon-button.css';
 type Props = {
   icon: IconType;
   disabled?: boolean;
+  color?: string;
 };
 
 type AllProps = Props & React.ComponentPropsWithoutRef<'button'>;
@@ -12,13 +13,14 @@ type AllProps = Props & React.ComponentPropsWithoutRef<'button'>;
 export function IconButton({
   icon: Icon,
   type = 'button',
+  color,
   ...restProps
 }: AllProps) {
   const classes = clsx('icon-button', restProps.className);
   return (
     <button {...restProps} type={type} className={classes}>
       <div className="icon-button__content">
-        <Icon size="1.5rem" className="icon-button__icon" />
+        <Icon size="1.5rem" className="icon-button__icon" color={color} />
       </div>
     </button>
   );
