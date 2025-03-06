99b994a99a1a753c1131c89c02730d641028ec74
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-05 21:38:33 -0500

    feat(web): update showcase

--- src/web/web.tsx
+++ src/web/web.tsx
@@ -1,11 +1,48 @@
-import { MdAdd } from 'react-icons/md';
+import { MdAdd, MdArrowBack, MdOutlineCancel } from 'react-icons/md';
 import { Button } from '../components/button/button';
+import { IconButton } from '../components/icon-button/icon-button';
+import { RadioGroup, Radio } from '../components/radio';
+import { TextField } from '../components/text-field/text-field';
 
 export function Web() {
   return (
     <div style={{ padding: '1rem' }}>
       <h1 className="text--display-large">Web - Alarmas Pico Placa</h1>
-      <p>Listado de botones:</p>
+      <p>Listado de componentes:</p>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <TextField
+          label="Label"
+          placeholder="Input"
+          description="Texto de soporte"
+          trailingIcon={<IconButton icon={MdOutlineCancel} />}
+        />
+        <TextField
+          label="Label"
+          placeholder="Input"
+          errorMessage="Texto de soporte"
+          isInvalid
+        />
+      </div>
+      <div
+        style={{
+          marginTop: '1rem',
+          display: 'flex',
+          gap: '1rem',
+        }}
+      >
+        <RadioGroup label="Choose your pet" name="pet" defaultValue="orni">
+          <Radio value="dogs">Dog</Radio>
+          <Radio value="cats">Cat</Radio>
+          <Radio value="dragon">Dragon</Radio>
+          <Radio isDisabled value="orni">
+            Ortinorrinco
+          </Radio>
+        </RadioGroup>
+      </div>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
+        <IconButton icon={MdArrowBack} />
+        <IconButton icon={MdArrowBack} disabled />
+      </div>
       <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem' }}>
         <Button text="Label" />
         <Button disabled text="Label" />
