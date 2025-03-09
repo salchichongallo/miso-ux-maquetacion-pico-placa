9d4f15f3c129865b04d64977ee9fd8b73b1683c9
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-09 16:33:38 -0500

    feat(web): login - validate form

--- src/web/pages/login/login.page.tsx
+++ src/web/pages/login/login.page.tsx
@@ -1,22 +1,36 @@
 import { MdArrowForward, MdDone } from 'react-icons/md';
 import { DialogTrigger, Link } from 'react-aria-components';
+import { useFormik } from 'formik';
 
 import { Modal } from '../../../components/modal/modal';
 import { Button } from '../../../components/button/button';
 import { TextField } from '../../../components/text-field/text-field';
 import { useNavigate } from 'react-router-dom';
+import { IconButton } from '../../../components/icon-button/icon-button';
 
 export function LoginPage() {
   const navigate = useNavigate();
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    navigate('/invalid-login');
-  };
+  const formik = useFormik({
+    initialValues: { username: '', password: '' },
+    initialTouched: { username: true, password: true },
+    validateOnChange: true,
+    onSubmit: () => navigate('/showcase'),
+    validate: values => {
+      const errors: Record<string, string> = {};
+      if (!values.username) {
+        errors.username = 'No puede estar vacío';
+      }
+      if (!values.password) {
+        errors.password = 'No puede estar vacío';
+      }
+      return errors;
+    },
+  });
   return (
     <div className="flex items-center flex-col justify-center h-screen p-3">
       <h1 className="text--headline-large mb-8">PICO • PLACA</h1>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={formik.handleSubmit}
         className="p-8 pt-6 rounded-xl w-full flex flex-col gap-6"
         style={{
           maxWidth: '22.0625rem',
@@ -24,11 +38,40 @@ export function LoginPage() {
         }}
       >
         <h2 className="text--title-medium text-center">Login</h2>
+
         <div className="flex flex-col gap-6">
-          <TextField label="Usuario" placeholder="Ingresa tu usuario" />
           <TextField
+            name="username"
+            validationBehavior="native"
+            value={formik.values.username}
+            onChange={formik.handleChange('username')}
+            isInvalid={!!formik.errors.username}
+            errorMessage={formik.errors.username}
+            label="Usuario"
+            placeholder={formik.errors.username ? '' : 'Ingresa tu usuario'}
+            trailingIcon={
+              formik.touched.username &&
+              !formik.errors.username &&
+              formik.values.username ? (
+                <IconButton icon={MdDone} />
+              ) : undefined
+            }
+          />
+          <TextField
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange('password')}
+            isInvalid={!!formik.errors.password}
+            errorMessage={formik.errors.password}
             label="Clave"
-            placeholder="Ingresa tu clave"
+            placeholder={formik.errors.password ? '' : 'Ingresa tu clave'}
+            trailingIcon={
+              formik.touched.password &&
+              !formik.errors.password &&
+              formik.values.password ? (
+                <IconButton icon={MdDone} />
+              ) : undefined
+            }
             type="password"
           />
         </div>
