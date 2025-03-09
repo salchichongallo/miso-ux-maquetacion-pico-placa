a1caf4cdb8c0a0e2f4f91a9e9d6cbb5eae0fa5dd
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-09 14:54:58 -0500

    feat: add tailwind support

--- vite.config.web.ts
+++ vite.config.web.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'node:path';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   root: 'src/web',
   build: {
     outDir: resolve(__dirname, 'dist/web'),
