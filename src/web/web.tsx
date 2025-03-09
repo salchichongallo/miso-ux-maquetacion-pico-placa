1183bc7507dbf0b93b64b4b7f8651958cff0699b
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-09 16:02:40 -0500

    feat(web): login - add invalid login page

--- src/web/web.tsx
+++ src/web/web.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Showcase } from '../components/showcase';
 import { LoginPage } from './pages/login/login.page';
+import { InvalidLoginPage } from './pages/login/invalid-login.page';
 
 export function Web() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/invalid-login" element={<InvalidLoginPage />} />
         <Route
           path="showcase"
           element={<Showcase title="Web - Alarmas Pico Placa" />}
