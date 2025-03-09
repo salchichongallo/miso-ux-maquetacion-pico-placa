ee3027f0c50bea53ee9580acc8d9bae015f74233
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-09 15:26:42 -0500

    feat(web): init login page

--- src/web/web.tsx
+++ src/web/web.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Showcase } from '../components/showcase';
+import { LoginPage } from './pages/login/login.page';
 
 export function Web() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route
-          path="/"
+          path="showcase"
           element={<Showcase title="Web - Alarmas Pico Placa" />}
         />
       </Routes>
