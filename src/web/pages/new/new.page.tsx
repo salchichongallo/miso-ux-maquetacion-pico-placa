958fb17e2fc34da2cee9a4201dbc7af6e0c7f486
Luis Alejandro Bogota <l.bogotab@uniandes.edu.co>
2025-03-10 19:38:09 -0500

    feat(web): new - add basic form fields

--- src/web/pages/new/new.page.tsx
+++ src/web/pages/new/new.page.tsx
@@ -1,6 +1,13 @@
-import { MdArrowBack } from 'react-icons/md';
-import { IconButton } from '../../../components/icon-button/icon-button';
+import { MdArrowBack, MdToday } from 'react-icons/md';
+import { ListBoxItem } from 'react-aria-components';
+
 import { AppLogo } from '../../components/app-logo';
+import { Button } from '../../../components/button/button';
+import { Divider } from '../../components/divider/divider';
+import { ComboBox } from '../../../components/combobox/combobox';
+import { TimeInput } from '../../../components/time-input/time-input';
+import { TextField } from '../../../components/text-field/text-field';
+import { IconButton } from '../../../components/icon-button/icon-button';
 
 export function NewPage() {
   return (
@@ -10,7 +17,7 @@ export function NewPage() {
           <AppLogo />
         </header>
         <main
-          className="flex flex-col gap-6 rounded-2xl"
+          className="flex flex-col rounded-2xl"
           style={{ background: 'var(--surface-container-low)' }}
         >
           <div className="pt-6 pr-8 pb-6 pl-6 flex gap-2 items-start">
@@ -22,6 +29,46 @@ export function NewPage() {
               </p>
             </div>
           </div>
+          <form className="px-8 flex flex-col gap-6">
+            <ComboBox label="Ciudad" placeholder="Seleccionar">
+              <ListBoxItem>Pasto</ListBoxItem>
+              <ListBoxItem>Medellín - Área Metropolitana</ListBoxItem>
+              <ListBoxItem>Bogotá - Área Metropolitana</ListBoxItem>
+              <ListBoxItem>Manizales</ListBoxItem>
+              <ListBoxItem>Cali</ListBoxItem>
+              <ListBoxItem>Pereira</ListBoxItem>
+            </ComboBox>
+
+            <ComboBox label="Tipo de vehículo" placeholder="Seleccionar">
+              <ListBoxItem>Carro particular</ListBoxItem>
+              <ListBoxItem>Moto 2 tiempos</ListBoxItem>
+              <ListBoxItem>Moto 4 tiempos</ListBoxItem>
+              <ListBoxItem>Taxi</ListBoxItem>
+            </ComboBox>
+
+            <div className="flex items-center gap-2">
+              <TimeInput label="Hora inicio" pickerTitle="Seleccionar" />
+              <Divider className="w-4 mt-4" />
+              <TimeInput label="Hora fin" pickerTitle="Seleccionar" />
+            </div>
+
+            <TextField
+              label="Vigente desde"
+              placeholder="Seleccionar"
+              trailingIcon={
+                <IconButton
+                  onPress={() => alert('TODO: open date picker')}
+                  icon={MdToday}
+                />
+              }
+            />
+
+            {/* TODO: add digits field */}
+
+            <div className="pt-6 pb-8 flex items-center justify-center">
+              <Button isDisabled type="submit" text="Crear pico y placa" />
+            </div>
+          </form>
         </main>
       </div>
     </div>
