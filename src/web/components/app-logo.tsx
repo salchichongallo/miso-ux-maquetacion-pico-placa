c95f7ac81e376244947b63efd92622d2cebbb923
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-03-11 06:18:46 -0500

    feat(logo): add skipNavigate prop

--- src/web/components/app-logo.tsx
+++ src/web/components/app-logo.tsx
@@ -1,9 +1,24 @@
 import { Link } from 'react-router-dom';
+import { usePress } from 'react-aria';
 
-export function AppLogo() {
+type Props = {
+  skipNavigate?: boolean;
+};
+
+export const AppLogo = function AppLogo({ skipNavigate = false }: Props) {
+  const { pressProps } = usePress({});
   return (
-    <Link to="/home" className="text--title-medium">
+    <Link
+      {...pressProps}
+      onClick={event => {
+        if (skipNavigate) {
+          event.preventDefault();
+        }
+      }}
+      to="/home"
+      className="text--title-medium"
+    >
       PICO • PLACA
     </Link>
   );
-}
+};
