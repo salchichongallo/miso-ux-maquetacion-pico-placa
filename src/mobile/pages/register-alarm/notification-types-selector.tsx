f4919ce0e0a719a94e606a07e4dc43891a746bb2
Luis Alejandro Bogota <l.bogotab@uniandes.edu.co>
2025-03-12 14:59:34 -0500

    feat(mobile): register-alarm - show modals

--- src/mobile/pages/register-alarm/notification-types-selector.tsx
+++ src/mobile/pages/register-alarm/notification-types-selector.tsx
@@ -1,19 +1,21 @@
 import { Radio, RadioGroup } from '../../../components/radio';
 
 type Props = {
+  value: string;
   onChange: (value: string) => void;
 };
 
-export function NotificationTypesSelector({ onChange }: Props) {
+export function NotificationTypesSelector({ value, onChange }: Props) {
   return (
     <div className="mt-4">
       <p className="text--label-large mb-1">
         Selecciona el tipo de notificación
       </p>
       <RadioGroup
-        label="Selecciona tu vehículo"
-        name="vehicle"
+        value={value}
         onChange={onChange}
+        label="Selecciona tu vehículo"
+        name="notificationType"
       >
         <Radio value="previous-day">El día anterior</Radio>
         <Radio value="before-begin">Antes de iniciar</Radio>
--- src/mobile/pages/register-alarm/register-alarm.page.tsx
+++ src/mobile/pages/register-alarm/register-alarm.page.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+import { MdCheck } from 'react-icons/md';
+
 import { AppShell } from '../../components/app-shell/app-shell';
+
+import { Modal } from '../../../components/modal/modal';
+import { Button } from '../../../components/button/button';
+import { TimePicker } from '../../../components/time-picker/time-picker';
 
 import { VehicleSelector } from './vehicle-selector';
 import { NotificationTypesSelector } from './notification-types-selector';
 
 export function RegisterAlarmPage() {
+  const [opened, setOpened] = useState(false);
+  const [modal, setModal] = useState<string>(null!);
   return (
     <AppShell>
       <div className="pt-14">
@@ -17,9 +26,83 @@ export function RegisterAlarmPage() {
               style={{ background: 'var(--surface-container-low)' }}
             >
               <VehicleSelector />
-              <NotificationTypesSelector onChange={console.info} />
+              <NotificationTypesSelector
+                value={modal}
+                onChange={value => {
+                  setModal(value);
+                  setOpened(true);
+                }}
+              />
             </div>
           </main>
+          <TimePicker
+            title="Indica una hora"
+            isOpen={opened && modal === 'previous-day'}
+            initialTime={null}
+            onOpenChange={isOpen => {
+              if (!isOpen) {
+                setOpened(isOpen);
+              }
+            }}
+            onOk={() => setModal('confirm-previous-day')}
+          />
+          <Modal
+            isOpen={opened && modal === 'confirm-previous-day'}
+            isDismissable={false}
+            title="Confirmar creación de alarma"
+            description="El día anterior - 7:00 a. m."
+            style={{ maxWidth: '19.5rem' }}
+            buttons={[
+              () => (
+                <Button
+                  onPress={() => setModal('previous-day')}
+                  text="No"
+                  variant="inline"
+                />
+              ),
+              () => <Button onPress={() => setModal('success')} text="Sí" />,
+            ]}
+          />
+          <Modal
+            isOpen={opened && modal === 'success'}
+            isDismissable={false}
+            title="Registro Exitoso"
+            icon={MdCheck}
+            style={{ maxWidth: '19.5rem' }}
+            buttons={[
+              () => (
+                <Button onPress={() => alert('TODO: redirect')} text="Sí" />
+              ),
+            ]}
+          />
+          <TimePicker
+            title="Indica un tiempo"
+            isOpen={opened && modal === 'before-begin'}
+            initialTime={null}
+            onOpenChange={isOpen => {
+              if (!isOpen) {
+                setOpened(isOpen);
+              }
+            }}
+            onOk={() => setModal('confirm-before-begin')}
+          />
+          <Modal
+            isOpen={opened && modal === 'confirm-before-begin'}
+            isDismissable={false}
+            title="Confirmar creación de alarma"
+            description="Antes de iniciar - 30 min"
+            style={{ maxWidth: '19.5rem' }}
+            buttons={[
+              () => (
+                <Button
+                  onPress={() => setModal('before-begin')}
+                  text="No"
+                  variant="inline"
+                />
+              ),
+              () => <Button onPress={() => setModal('success')} text="Sí" />,
+            ]}
+          />
         </div>
       </div>
     </AppShell>
